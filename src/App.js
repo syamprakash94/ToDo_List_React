2c4328d98e4c1146e2859c34e9682f29fe53b3fe
syam prakash <syamprakash94@gmail.com>
2022-06-20 18:36:00 +0530

    third commmit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ function App() {
   const [toDos, setToDos] = useState([])
   const [toDo, setToDo] = useState('')
 
-  const deleteTask=(index) =>{
+  // to delete a field
+  const deleteTask = (index) => {
     const ask = window.confirm("dlete");
-    if(ask) {
+    if (ask) {
       const test = [...toDos];
-      // const cut = test.slice(index,1)
-      test.splice(index,1)
+      test.splice(index, 1)
       setToDos(test);
-    }else{
-      
+    } else {
     }
   }
 
-
+  // to erase the main field
   const eraser = () => {
     setToDo("");
   };
+
   return (
     <div className="app">
+
       <div className="mainHeading">
         <h1>ToDo List</h1>
       </div>
@@ -30,23 +31,30 @@ function App() {
         <br />
         <h2>Let's sort your work! 🌝 ☕ </h2>
       </div>
-      <div className="input">
-        <input value={toDo} 
-        onChange={(e) => setToDo(e.target.value)} 
-        type="text" 
-        placeholder="🖊️ Add item..." />
-        
-        <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
-     <i class="fa-solid fa-eraser" onClick={eraser}></i>
-      </div>
-      <div className="todos">
-        {toDos.map((obj,index) => {
 
+
+      <div className="input">
+        <input value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..." />
+        <button style={{border:"none", backgroundColor:"white"}} type="submit">
+
+          <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
+
+        </button>
+
+        <i class="fa-solid fa-eraser" onClick={eraser}></i>
+      </div>
+
+
+      <div className="todos">
+        {toDos.map((obj, index) => {
           return (<div className="todo">
             <div className="left">
               <input onChange={(e) => {
-                console.log(e.target.value);
-                console.log(obj);
+                // console.log(e.target.value);
+                // console.log(obj);
                 setToDos(toDos.filter(obj2 => {
                   if (obj2.id === obj.id) {
                     obj2.status = e.target.checked
@@ -58,34 +66,22 @@ function App() {
               <p>{obj.text}</p>
             </div>
             <div className="right">
-              <i  className="fas fa-times" onClick={()=> deleteTask(index)}></i>
-
-
-              
-              
-              
+              <i className="fas fa-times" onClick={() => deleteTask(index)}></i>
             </div>
           </div>)
         })}
 
 
-
-
-<div className="input2">
-
-        {toDos.map((obj) => {
-          if (obj.status) {
-            return (
-            <h1>{obj.text}</h1>
-            )
-          }
-          return null
-        })}
- </div>
- 
-
-
-
+        <div className="input2">
+          {toDos.map((obj) => {
+            if (obj.status) {
+              return (
+                <h1>{obj.text}</h1>
+              )
+            }
+            return null
+          })}
+        </div>
       </div>
     </div>
   );
